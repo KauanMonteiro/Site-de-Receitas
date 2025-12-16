bf1c9700dcd1d059c867784316aedb376b4c2ccf
Kauan <kauanmonteirosilva789@gmail.com>
2025-12-16 15:42:56 -0300

    Adicionado sistema de mensagens

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,20 @@
+import os
+
 from django.shortcuts import render, get_object_or_404,get_list_or_404
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
 from recipes.models import Recipe
 from django.db.models import Q
 from utils.pagination import make_pagination
-
-import os
+from django.contrib import messages
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
 def home(request):
     recipes = Recipe.objects.filter( is_published=True).order_by('-id')
-    page_obj,pagination_range = make_pagination(request,recipes,PER_PAGE) 
+    page_obj,pagination_range = make_pagination(request,recipes,PER_PAGE)
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
         'pagination_range':pagination_range
