420733a6830f1fd7ccf8335906d55470fa7d1e61
Kauan <kauanmonteirosilva789@gmail.com>
2025-12-22 17:30:07 -0300

    Adicionado formulário de registro

--- authors/views.py
+++ authors/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import redirect, render
+from .forms import RegisterForm
 
 def register_view(request):
-    return render(request,'pages/register_view.html')
+    form = RegisterForm()
+    return render(request,'pages/register_view.html',{
+        'form':form
+    })
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+    
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+    return redirect('authors:register')
