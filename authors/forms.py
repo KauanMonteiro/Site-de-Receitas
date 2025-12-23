b6baf4377c3f1a8405b2ee4ce516df7ba31b6061
Kauan <kauanmonteirosilva789@gmail.com>
2025-12-23 18:02:42 -0300

    Adiciona validação de senha e melhorias no formulário de registro

--- authors/forms.py
+++ authors/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-
+from django.core.exceptions import ValidationError
 
 def add_attr(field, attr_name, attr_new_val):
     existing = field.widget.attrs.get(attr_name, '')
@@ -18,7 +18,7 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['first_name'], 'Ex.: John')
         add_placeholder(self.fields['last_name'], 'Ex.: Doe')
         
-        password = forms.CharField(
+    password = forms.CharField(
         required=True,
         widget=forms.PasswordInput(attrs={
             'placeholder': 'Your password'
@@ -71,3 +71,25 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Type your password here'
             })
         }
+
+
+    def clean_password(self):
+        data = self.cleaned_data.get('password')        
+        if len(data) <8 :
+            raise ValidationError(
+                'Senha muito curta',
+                code='invalid'
+            )
+        return data
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            raise ValidationError(
+                {'password':'Password and password2 must be equal','password2':'Password and password2 must be equal',}
+            )
+
+
--- authors/views.py
+++ authors/views.py
@@ -1,18 +1,21 @@
 from django.http import Http404
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
 
 def register_view(request):
     form = RegisterForm()
-    return render(request,'pages/register_view.html',{
-        'form':form
-    })
+    return render(request, 'pages/register_view.html', {'form': form})
+
 
 def register_create(request):
-    if not request.POST:
+    if request.method != 'POST':
         raise Http404()
-    
-    POST = request.POST
-    request.session['register_form_data'] = POST
-    form = RegisterForm(POST)
+
+    form = RegisterForm(request.POST)
+
+    if not form.is_valid():
+        return render(request, 'pages/register_view.html', {
+            'form': form
+        })
+
     return redirect('authors:register')
